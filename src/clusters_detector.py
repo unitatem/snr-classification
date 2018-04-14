5544e188c8bd5796f260f82cba119fadc56a0623
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-04-14 14:48:34 +0200

    Transform data from feature space into cluster space

--- src/clusters_detector.py
+++ src/clusters_detector.py
@@ -8,16 +8,19 @@ from src import config
 
 
 def load_database(db_path):
+    logging.info("Loading data")
     features_db = dir_archive(db_path, {}, serialized=True, cached=True)
     features_db.load()
     return features_db
 
 
 def concatenate_data(original):
+    logging.info("Concatenating data")
     data = np.array([], dtype=np.float32)
     data.shape = (0, 128)
-    for batch in features_db:
-        data = np.concatenate((data, features_db[batch]))
+    for class_name in features_db:
+        for photo_name in features_db[class_name]:
+            data = np.concatenate((data, features_db[class_name][photo_name]))
     return data
 
 
@@ -37,13 +40,25 @@ def find_clusters(data, clusters_count):
 if __name__ == "__main__":
     logging.basicConfig(filename="clusters.log", level=logging.DEBUG)
 
+    print("Loading data")
     features_db = load_database(config.features_db_path)
+    print("Concatenating data")
     data = concatenate_data(features_db)
+    print("kMeans running")
     kmeans = find_clusters(data, config.clusters_count)
 
-    # print("labels:")
-    # print(kmeans.labels_)
-    # print("predict:")
-    # print(kmeans.predict([[0, 0], [4, 4]]))
-    print("centers:")
-    print(kmeans.cluster_centers_)
+    # print("centers:")
+    # print(kmeans.cluster_centers_)
+
+    print("Changing space from features into clusters")
+    cluster_db = dir_archive(config.clusters_db_path, {}, serialized=True, cached=False)
+    for class_name in features_db:
+        print(class_name)
+        cluster_batch = {}
+        for photo_name in features_db[class_name]:
+            labels = kmeans.predict(features_db[class_name][photo_name])
+
+            cluster_batch[photo_name] = np.zeros(shape=config.clusters_count, dtype=np.int16)
+            for label in labels:
+                cluster_batch[photo_name][label] += 1
+        cluster_db[class_name] = cluster_batch
--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 take_every_nth_sample = 4
-clusters_count = 10
+clusters_count = 8
 
 resources_path = "../resources/"
 
@@ -7,3 +7,4 @@ set_path = resources_path + "SET_B/"
 bounding_boxes_path = resources_path + "bounding_boxes.txt"
 
 features_db_path = resources_path + "extracted_features_" + str(take_every_nth_sample) + ".db"
+clusters_db_path = resources_path + "clusters_" + str(take_every_nth_sample) + "_" + str(clusters_count) + ".db"
