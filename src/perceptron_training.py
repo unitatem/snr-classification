d63150112a3cd437f54a51667f8509cb5c9fd84d
Pawel Karwacki <pawel.witcher@wp.pl>
2018-04-16 22:46:51 +0200

    Add divide_data() function

--- src/perceptron_training.py
+++ src/perceptron_training.py
@@ -1,7 +1,9 @@
 import logging
+import h5py
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from src import config
+from random import shuffle
 
 
 def build_perceptron():
@@ -12,7 +14,26 @@ def build_perceptron():
             model.add(Dense(layer_size, input_dim=config.clusters_count))
         else:
             model.add(Dense(layer_size))
+        model.add(Activation('sigmoid'))
+    cluster_db = h5py.File(config.clusters_db_path, 'r')
+    output_size = len(cluster_db)
+    model.add(Dense(output_size))
+    model.add(Activation('softmax'))
+    cluster_db.close()
     return model
+
+
+def divide_data(training_fraction):
+    cluster_db = h5py.File(config.clusters_db_path, 'r')
+    data_ids = []
+    for class_name in cluster_db:
+        for photo_name in cluster_db[class_name]:
+            data_ids.append((class_name, photo_name))
+    cluster_db.close()
+
+    shuffle(data_ids)
+    divide_point = int(len(data_ids)*training_fraction)
+    return data_ids[:divide_point], data_ids[divide_point:]
 
 
 if __name__ == '__main__':
