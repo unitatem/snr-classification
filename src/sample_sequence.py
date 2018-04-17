99f8c7b5f9630ad2f89999f70b6645a5b36f7ff3
Pawel Karwacki <pawel.witcher@wp.pl>
2018-04-17 14:26:17 +0200

    Fix bug in SampleSequence class
    Due to premature db closure returned objects were unavailable

--- src/sample_sequence.py
+++ src/sample_sequence.py
@@ -8,6 +8,7 @@ class SampleSequence(utils.Sequence):
     def __init__(self, sample_ids, sample_classes, batch_size):
         self.ids, self.classes = sample_ids, sample_classes
         self.batch_size = batch_size
+        self.cluster_db = h5py.File(config.clusters_db_path, 'r')
 
     def __len__(self):
         return int(np.ceil(len(self.ids)) / float(self.batch_size))
@@ -16,7 +17,8 @@ class SampleSequence(utils.Sequence):
         batch_x = self.ids[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y = self.classes[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-        cluster_db = h5py.File(config.clusters_db_path, 'r')
-        batch_x = [cluster_db[x[0]][x[1]] for x in batch_x]
-        cluster_db.close()
+        batch_x = [self.cluster_db[x[0]][x[1]] for x in batch_x]
         return np.array(batch_x), np.array(batch_y)
+
+    def close(self):
+        self.cluster_db.close()
